9b4606a8d1a7d7fcb3a33e6e0caa0181641dc695
kbychkov <k.bychkov@yandex.ru>
2018-03-03 10:52:19 +0300

    Update 2018-03-03T07:52:14.692Z

--- app/scripts/app.js
+++
@@ -1,6 +0,0 @@
-import $ from 'jquery';
-import svg4everybody from 'svg4everybody';
-
-$(() => {
-  svg4everybody();
-});
--- tasks/copy.js
+++
@@ -1,9 +0,0 @@
-const gulp = require('gulp');
-const changed = require('gulp-changed');
-
-gulp.task('copy', () => {
-  gulp
-    .src('app/resources/**/*')
-    .pipe(changed('dist'))
-    .pipe(gulp.dest('dist'));
-});
